32e413d509bcd8f2427d48152e6a6cb01472a9e2
Rachel Le <71084940+Rachel10011@users.noreply.github.com>
2023-04-11 15:58:57 -0400

    Made error

---
+++ BackEnd/routes/clientRoute.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const { Model, model } = require("mongoose");
+const router = express.Router();
+const Student = require("../models/client");
---
+++ BackEnd/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+app.use(express.static(__dirname + '/public'));
+
+const clientsRouter = require('./routes/clientRoute')
+
+app.use('/clients', clientsRouter)
+
+// const itemsRouter = require('./routes/itemRoute')
+
+// app.use('/items', itemsRouter)
+
+app.listen(3000, () => console.log('Server Started'))
