fc48888d20a008205e20f93bd7ba1b4e07d4e991
Rachel Le <71084940+Rachel10011@users.noreply.github.com>
2023-04-12 17:19:24 -0400

    Added client routes from Nav branch

--- BackEnd/routes/clientRoute.js
+++ BackEnd/routes/clientRoute.js
@@ -4,32 +4,61 @@ const router = express.Router();
 const Client = require("../models/client");
 
 router.post("/register", async (req, res) => {
-//   console.log("register");
-//   const client = await Client.findOne({ email: req.body.email });
-//   if (!client) {
-//     console.log("register success");
-//     const client = new Client({
-//       email: req.body.email,
-//       password: req.body.password,
-//     });
-//     try {
-//       await client.save();
-//     } catch (err) {
-//       res.status(400).json({ message: err.message });
-//     }
-//     const registerStatus =
-//       "Account creation successful! Please login with these credentials";
-//     res.status(201).json({ registerStatus });
-//   } else {
-//     console.log("register fail");
-//     const registerStatus =
-//       "Sorry, that username already exists. Please try again";
-//     res.status(201).json({ registerStatus });
-//   }
+  console.log("register");
+  
+  const client = await Client.findOne({ email: req.body.email });
+  
+  if (!client) {
+    console.log("register success");
+    const client = new Client({
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-res.status(201)
+    try {
+      await client.save();
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+    const registerStatus =
+      "Account creation successful! Please login with these credentials";
+    res.status(201).json({ registerStatus });
+  } else {
+    console.log("register fail");
+    const registerStatus =
+      "Sorry, that username already exists. Please try again";
+    res.status(401).json({ registerStatus });
+  }
+
+});
+
+router.post("/login", async (req, res) => {
+  const client = await Client.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  //was tryin query params wasn't workin
+//   tried router.get("/login/:email:password") and ("/login/email/:email/password/:password")
+    // console.log(req.params)
+
+    // const client = await Client.findOne({
+    //     email: req.params.email,
+    //     password: req.params.password
+    // })
+
+
+  if (client != null) {
+    console.log("User exists");
+    const email = { email: client.email };
+
+    res.status(201).json(email.email + " has successfully logged in");
+   
+  } else {
+    console.log("User does not exist");
+    res.status(401).json("Email or password is incorrect, please try again");
+  }
 });
 
 
 module.exports = router;
-
