a41dc7c7a6b0eef55d74a8aa490ceaa3f9866a75
Navdeep Mangat <Nmangat0337@conestogac.on.ca>
2023-04-12 12:33:07 -0400

    env and startin my routes

--- BackEnd/routes/clientRoute.js
+++ BackEnd/routes/clientRoute.js
@@ -1,4 +1,35 @@
 const express = require("express");
 const { Model, model } = require("mongoose");
 const router = express.Router();
-const Student = require("../models/client");
+const Client = require("../models/client");
+
+router.post("/register", async (req, res) => {
+//   console.log("register");
+//   const client = await Client.findOne({ email: req.body.email });
+//   if (!client) {
+//     console.log("register success");
+//     const client = new Client({
+//       email: req.body.email,
+//       password: req.body.password,
+//     });
+//     try {
+//       await client.save();
+//     } catch (err) {
+//       res.status(400).json({ message: err.message });
+//     }
+//     const registerStatus =
+//       "Account creation successful! Please login with these credentials";
+//     res.status(201).json({ registerStatus });
+//   } else {
+//     console.log("register fail");
+//     const registerStatus =
+//       "Sorry, that username already exists. Please try again";
+//     res.status(201).json({ registerStatus });
+//   }
+
+res.status(201)
+});
+
+
+module.exports = router;
+
