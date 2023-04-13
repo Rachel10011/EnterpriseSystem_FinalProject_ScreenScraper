c4024bcbb9237ad525bcc3fda21027ed9a0c9008
Rachel Le <71084940+Rachel10011@users.noreply.github.com>
2023-04-13 01:43:07 -0400

    updated backgroundservice

--- BackgroundService/notification.py
+++ BackgroundService/notification.py
@@ -1,37 +1,126 @@
-import schedule
-import time as tm
 import smtplib
 import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from pymongo import MongoClient
+import config
+import requests
+import time
 
-connection_string = "mongodb+srv://scraper:newpassword@cluster0.6bx67x8.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(connection_string)
-
-# Database Name
-db = client["Scraper"]
-
-# Collection Name
-col = db["items"]
-
-x = col.find({}, {
-    '_id': 0,
-    'itemName': 1,
-    'originalPrice': 1,
-    'newPrice': 1,
-    'rating': 1,
-    'reviewTotal': 1,
-    'availability': 1,
-    'url': 1,
-    'email': 1
-})
-
-for data in x:
-    print(data)
+# Connect to the database and get the right table
 
 
-# def job():
+def get_database():
+
+    CONNECTION_STRING = config.dbURL
+
+    client = MongoClient(CONNECTION_STRING)
+
+    return client['Scraper']
+
+
+def check(item):
+    body = {"URL": item['url']}
+    scraperUrl = "http://localhost:5000/scrape"
+
+    post_response = requests.post(scraperUrl, json=body)
+    post_response_json = post_response.json()
+    temp = post_response_json['Price']
+    price = float(temp[1:])
+    # print('Original Price is: ' + "%.2f" % float(item['originalPrice']))
+    # print('Scraper Price is: ' + "%.2f" % price)
+
+    if float(item['originalPrice']) > price and float(item['newPrice']) != price:
+        # print('Update Db and send Email')
+        # print('Price is lower but db isnt updated and email not sent yet')
+        return price
+    elif float(item['originalPrice']) > price and float(item['newPrice']) == price:
+        # print('Already Updateed Db and sent Email')
+        return 0
+    else:
+        # print('No Change')
+        return 0
+
+
+def sendEmail(item):
+    sender_email = config.email
+    receiver_email = item['email']
+    password = config.emailPW
+
+    url = item['url']
+    newPrice = item['newPrice']
+    name: item['itemName']
+    oldPrice: item['originalPrice']
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "DISCOUNTS CHECK"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    # Create the plain-text and HTML version of your message
+    text = """\
+    SALE:
+    Check out the big discount on your added item.
+    Items: {sendName}: {sendurl}
+    Current Price: {sendNewPrice}
+    Original Price: {sendOldPrice}
+    """  # .format(sendName=name, sendurl=url, sendNewPrice=newPrice, sendOldPrice=oldPrice)
+
+    html = """\
+    <html>
+        <body>
+        <p>SALE:<br>
+                Check out the big discount on your added item.<br>
+        </p>
+        <div> 
+            Items: "{name}": "{url}"<br>
+            Current Price: "{newPrice}"<br>
+            Original Price: "{newPrice}"<br>
+        </div>
+        </body>
+    </html>
+    """  # .format(sendName=name, sendurl=url, sendNewPrice=newPrice, sendOldPrice=newPrice)
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
+
+
+if __name__ == "__main__":
+    # while True:
+    dbname = get_database()
+    items = dbname.items
+    for item in items.find():
+        newPrice = check(item)
+        if newPrice != 0:
+            print('========Email')
+            # add function call for sending emails here
+            sendEmail(item)
+
+            print('Add and Email')
+            item['newPrice'] = newPrice
+            items.update_one({'_id': item['_id']}, {
+                             "$set": item}, upsert=False)
+
+        else:
+            print('No Change')
+    # time.sleep(21600)
+
+
+# def sendEmail(item):
 #     sender_email = "gooddeals8998@gmail.com"
 #     receiver_email = "letra1712@gmail.com"
 #     password = "ikovsnbkhaufjjpf"
@@ -72,10 +161,3 @@ for data in x:
 #         server.sendmail(
 #             sender_email, receiver_email, message.as_string()
 #         )
-
-
-# schedule.every(5).seconds.do(job)
-
-# while True:
-#     schedule.run_pending()
-#     tm.sleep(1)
