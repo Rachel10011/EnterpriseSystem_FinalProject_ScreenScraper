52903552f33dbd7cbeed267e4a48a1a74b7573d3
Navdeep Mangat <Nmangat0337@conestogac.on.ca>
2023-04-12 21:59:52 -0400

    Added scraper to the project

---
+++ PythonScraper/app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import scraper
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "content-Type"
+
+
+@app.route('/')
+def default():
+    online = {"Status": "Online"}
+    return json.dumps(online)
+
+
+@app.route('/scrape', methods=['POST'])
+@cross_origin()
+def get_product_details():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        body = request.json
+        results = scraper.scrape(body["URL"])
+        return results
+    else:
+        return 'Content-Type not supported!'
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0",)
---
+++ PythonScraper/app/scraper.py
@@ -0,0 +1,104 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+ 
+def scrape(URL):
+    # Dict for storing product details
+    product = {
+        "Name": "",
+        "Price": 0,
+        "Rating": "",
+        "ReviewCount": "",
+        "Availability": ""
+    }
+ 
+    # specifying user agent, You can use other user agents
+    # available on the internet
+    HEADERS = ({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64)AppleWebKit/537.36 (KHTML, like Gecko)Chrome/44.0.2403.157 Safari/537.36',
+                                'Accept-Language': 'en-US, en;q=0.5'})
+ 
+    # Making the HTTP Request
+    webpage = requests.get(URL, headers=HEADERS)
+ 
+    # Creating the Soup Object containing all data
+    soup = BeautifulSoup(webpage.content, "lxml")
+ 
+    # retrieving product title
+    try:
+        # Outer Tag Object
+        title = soup.find("span",
+                          attrs={"id": 'productTitle'})
+ 
+        # Inner NavigableString Object
+        title_value = title.string
+ 
+        # Title as a string value
+        title_string = title_value.strip().replace(',', '')
+ 
+    except AttributeError:
+        title_string = "NA"
+ 
+    # saving the Name in the dict
+    product["Name"] = title_string
+    #print("Product Name = ", product["Name"])
+ 
+   # Retreivng Price
+    try:
+        price = soup.find("span", class_="a-offscreen").text
+        
+    except AttributeError:
+        try:
+            price = soup.find("span", attrs={"id": 'sns-base-price'}).text
+            if price.find('\n') != -1:
+                priceSplit = price.split('\n')
+                price = priceSplit[0].strip()
+            # we are omitting unnecessary spaces
+            # and commas form our string   
+        except AttributeError:
+            price = "NA"
+ 
+    # saving price to dict
+    product["Price"] = price
+    #print("Products price = ", price)
+ 
+    # retrieving product rating
+    try:
+        rating = soup.find("i", attrs={'class': 'a-icon a-icon-star a-star-4-5'}).string.strip().replace(',', '')
+ 
+    except AttributeError:
+ 
+        try:
+            rating = soup.find(
+                "span", attrs={'class': 'a-icon-alt'}).string.strip().replace(',', '')
+        except:
+            rating = "NA"
+ 
+    # Saving ratings to dict
+    product["Rating"] = rating
+    #print("Overall rating = ", rating)
+ 
+    try:
+        review_count = soup.find(
+            "span", attrs={'id': 'acrCustomerReviewText'}).string.strip().replace(',', '')
+ 
+    except AttributeError:
+        review_count = "NA"
+
+    # Save ReviewCount to dict
+    product["ReviewCount"] = review_count
+    #print("Total reviews = ", review_count)
+
+    # print availablility status
+    try:
+        available = soup.find("div", attrs={'id': 'availability'})
+        available = available.find("span").string.strip().replace(',', '')
+ 
+    except AttributeError:
+        available = "NA"
+ 
+    # Saving the availability
+    product["Availability"] = available
+    #print("Availability = ", available)
+ 
+    # Return the product as a JSON object
+    return json.dumps(product)
